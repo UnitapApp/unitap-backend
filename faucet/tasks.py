11d516eb3445dd3966264d4eb8150620d82bf90d
Ali Parvizi <snparvizi75@gmail.com>
2022-06-13 08:45:26 -0400

    independant pending tx process based on chain

--- faucet/tasks.py
+++ faucet/tasks.py
@@ -26,24 +26,29 @@ def has_pending_receipt_with_tx_hash(chain):
 
 
 @shared_task
+def proccess_chain_pending_receipts(chain_id):
+    with transaction.atomic():
+        chain = Chain.objects.select_for_update().get(pk=chain_id)  # lock based on chain
+
+        # all pending receipts with a tx_hash must be resolved before new transactions can be made
+        if has_pending_receipt_with_tx_hash(chain):
+            return
+
+        receipts = ClaimReceipt.objects.filter(chain=chain,
+                                                _status=ClaimReceipt.PENDING,
+                                                tx_hash=None)
+        if receipts.count() == 0:
+            return
+
+        # generate transfer data in batches of 32
+        data = [{'to': receipt.bright_user.address, 'amount': receipt.amount} for receipt in receipts[:32]]
+
+        manager = EVMFundManager(chain)
+        tx_hash = manager.multi_transfer(data)
+        receipts.update(tx_hash=tx_hash)
+
+
+@shared_task
 def process_pending_receipts_with_no_hash():  # periodic task
     for _chain in Chain.objects.all():
-        with transaction.atomic():
-            chain = Chain.objects.select_for_update().get(pk=_chain.pk)  # lock based on chain
-
-            # all pending receipts with a tx_hash must be resolved before new transactions can be made
-            if has_pending_receipt_with_tx_hash(chain):
-                continue
-
-            receipts = ClaimReceipt.objects.filter(chain=chain,
-                                                   _status=ClaimReceipt.PENDING,
-                                                   tx_hash=None)
-            if receipts.count() == 0:
-                continue
-
-            # generate transfer data in batches of 32
-            data = [{'to': receipt.bright_user.address, 'amount': receipt.amount} for receipt in receipts[:32]]
-
-            manager = EVMFundManager(chain)
-            tx_hash = manager.multi_transfer(data)
-            receipts.update(tx_hash=tx_hash)
+        proccess_chain_pending_receipts.delay(_chain.pk)
