a3867382bb9eaf2735f44218bce740197e616b15
Ali Parvizi <snparvizi75@gmail.com>
2022-10-23 15:51:24 +0330

    non-blocking uwsgi run

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
 python manage.py migrate
-uwsgi --socket 0.0.0.0:5678 --protocol=http -w brightIDfaucet.wsgi
+uwsgi --socket 0.0.0.0:5678 --protocol=http -w brightIDfaucet.wsgi &
 celery -A brightIDfaucet worker -B
