152a0c5e94b25da202f0a378c7b67022dbb86093
mohamadbastin <mohamadbastin@gmail.com>
2023-03-12 12:56:49 +0330

    edit claim model to save old bright user

--- faucet/migrations/0032_remove_claimreceipt_bright_user_and_more.py
+++ faucet/migrations/0032_remove_claimreceipt_bright_user_and_more.py
@@ -5,26 +5,35 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('authentication', '0008_alter_userprofile_user_alter_wallet_user_profile'),
-        ('faucet', '0031_chain_is_active'),
+        ("authentication", "0008_alter_userprofile_user_alter_wallet_user_profile"),
+        ("faucet", "0031_chain_is_active"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='claimreceipt',
-            name='bright_user',
+            model_name="claimreceipt",
+            name="bright_user",
         ),
         migrations.AddField(
-            model_name='claimreceipt',
-            name='user_profile',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='claims', to='authentication.userprofile'),
+            model_name="claimreceipt",
+            name="user_profile",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name="claims",
+                to="authentication.userprofile",
+            ),
             preserve_default=False,
         ),
         migrations.AlterField(
-            model_name='chain',
-            name='chain_type',
-            field=models.CharField(choices=[('EVM', 'EVM'), ('NONEVM', 'Non-EVM')], default='EVM', max_length=10),
+            model_name="chain",
+            name="chain_type",
+            field=models.CharField(
+                choices=[("EVM", "EVM"), ("NONEVM", "Non-EVM")],
+                default="EVM",
+                max_length=10,
+            ),
         ),
     ]
