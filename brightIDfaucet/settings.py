507735e3e2152817674fb903dc4552cc0ae8f06c
mohamadbastin <mohamadbastin@gmail.com>
2023-04-21 13:44:42 +0330

    add logging config to settings.py

--- brightIDfaucet/settings.py
+++ brightIDfaucet/settings.py
@@ -5,12 +5,16 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from dotenv import load_dotenv
 import dj_database_url
+import logging
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
