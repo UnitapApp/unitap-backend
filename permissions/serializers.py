9e6110f993092266810ddc566709a75a408c8c94
mohamadbastin <mohamadbastin@gmail.com>
2023-05-20 18:00:50 +0330

    fix typo

--- permissions/serializers.py
+++ permissions/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from rest_polymorphic.serializers import PolymorphicSerializer
 
 
-
 from permissions.models import (
     Permission,
     BrightIDMeetVerification,
