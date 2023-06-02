f4d7c2639d0e4b57d6b45535ae200dd9a921c881
Mohamad <mohamad@Mohamads-MacBook-Pro.local>
2023-06-02 13:08:57 +0330

    weekle/monthly permissions

--- permissions/admin.py
+++ permissions/admin.py
@@ -12,5 +12,15 @@ class BrightIDAuraVerificationAdmin(admin.ModelAdmin):
     list_display = ["pk", "name"]
 
 
+class OncePerWeekVerificationAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name"]
+
+
+class OncePerMonthVerificationAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name"]
+
+
 admin.site.register(BrightIDMeetVerification, BrightIDMeetVerificationAdmin)
 admin.site.register(BrightIDAuraVerification, BrightIDAuraVerificationAdmin)
+admin.site.register(OncePerWeekVerification, OncePerWeekVerificationAdmin)
+admin.site.register(OncePerMonthVerification, OncePerMonthVerificationAdmin)
