7b34f207b44548a242065022d24456ec8a772fd8
mohamadbastin <mohamadbastin@gmail.com>
2023-06-09 20:09:58 +0330

    raffle api test

--- prizetap/tests.py
+++ prizetap/tests.py
@@ -90,3 +90,55 @@ class RaffleTestCase(APITestCase):
         raffle.save()
 
         self.assertTrue(raffle.is_expired)
+
+
+class RaffleAPITestCase(APITestCase):
+    def setUp(self) -> None:
+        self.chain = Chain.objects.create(
+            chain_name="Gnosis Chain",
+            wallet=WalletAccount.objects.create(
+                name="Gnosis Chain Wallet",
+                private_key=test_wallet_key,
+                network_type=NetworkTypes.EVM,
+            ),
+            rpc_url_private=test_rpc_url_private,
+            fund_manager_address=fund_manager,
+            native_currency_name="xdai",
+            symbol="XDAI",
+            chain_id="100",
+            explorer_url="https://blockscout.com/poa/xdai/",
+            max_claim_amount=x_dai_max_claim,
+            tokentap_contract_address=gnosis_tokentap_contract_address,
+        )
+
+        self.permission = BrightIDMeetVerification.objects.create(
+            name="BrightID Meet Verification"
+        )
+        self.permission2 = BrightIDAuraVerification.objects.create(
+            name="BrightID Aura Verification"
+        )
+
+        raffle = Raffle.objects.create(
+            name="Test Raffle",
+            description="Test Raffle Description",
+            creator="Test Creator",
+            is_prize_nft=False,
+            prize="Test Prize",
+            chain=self.chain,
+            deadline=timezone.now() + timezone.timedelta(days=1),
+            max_number_of_entries=100,
+        )
+
+        raffle.permissions.set([self.permission, self.permission2])
+
+    def test_raffle_list(self):
+        url = reverse("raffle-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+        # check permissions count
+        self.assertEqual(len(response.data[0]["permissions"]), 2)
+        # check second permission name
+        self.assertEqual(
+            response.data[0]["permissions"][1]["name"], "BrightID Aura Verification"
+        )
