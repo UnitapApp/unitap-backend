d6defb432461e8c01c3ccb51632f7b8a1acda85e
Shayan Shiravani <shayan5420@yahoo.com>
2023-07-04 13:19:31 +0330

    User constraints

--- core/constraints.py
+++ core/constraints.py
@@ -1,17 +1,24 @@
+from abc import ABC, abstractmethod
 from authentication.models import UserProfile
 
-class BrightIDMeetVerification():
-    def is_passed(self, user_profile: UserProfile, *args, **kwargs):
-        return user_profile.is_meet_verified
+class ConstraintVerification(ABC):
+    def __init__(self, user_profile:UserProfile, response:str = None) -> None:
+        self.user_profile = user_profile
+        self.response_text = response
 
-    def response(self):
-        return "You must be verified on BrightID"
+    @abstractmethod
+    def is_observed(self, *args, **kwargs):
+        pass
+
+    def response(self) -> str:
+        return self.response_text or f"{self.__class__.__name__} constraint is violated"
+
+class BrightIDMeetVerification(ConstraintVerification):
+    def is_observed(self, *args, **kwargs):
+        return self.user_profile.is_meet_verified
 
 
-class BrightIDAuraVerification():
-    def is_passed(self, user_profile: UserProfile, *args, **kwargs):
-        return user_profile.is_aura_verified
-
-    def response(self):
-        return "You must be Aura verified on BrightID"
+class BrightIDAuraVerification(ConstraintVerification):
+    def is_observed(self, *args, **kwargs):
+        return self.user_profile.is_aura_verified
     
