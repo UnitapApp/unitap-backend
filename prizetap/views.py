7dd9a68257aa5215f99e9b0e19096b70d9d3c374
Shayan Shiravani <shayan5420@yahoo.com>
2023-07-05 00:20:39 +0330

    ClaimPrize validator

--- prizetap/views.py
+++ prizetap/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-import rest_framework.exceptions
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView,CreateAPIView
 from rest_framework.permissions import IsAuthenticated
@@ -9,6 +8,7 @@ from .models import Raffle, RaffleEntry
 from .serializers import RaffleSerializer, RaffleEntrySerializer
 from .validators import (
     RaffleEnrollmentValidator,
+    ClaimPrizeValidator,
     SetRaffleEntryTxValidator
 )
 
@@ -53,35 +53,16 @@ class RaffleEnrollmentView(CreateAPIView):
 class ClaimPrizeView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def can_claim_prize(self, raffle: Raffle, user_profile: UserProfile):
-        if not raffle.is_expired:
-            raise rest_framework.exceptions.PermissionDenied(
-                "The raffle is not over"
-            )
-        if not raffle.winner or raffle.winner != user_profile:
-            raise rest_framework.exceptions.PermissionDenied(
-                "You are not the raffle winner"
-            )
-        
-    def check_user_permissions(self, raffle: Raffle, user_profile):
-        for permission in raffle.permissions.all():
-            permission: Permission
-            if not permission.is_valid(
-                user_profile, raffle=raffle
-            ):
-                raise rest_framework.exceptions.PermissionDenied(
-                    permission.response()
-                    if permission.response() is not None
-                    else "You do not have permission to enroll in this raffle"
-                )
-
     def post(self, request, pk):
         user_profile = request.user.profile
         raffle = get_object_or_404(Raffle, pk=pk)
         
-        self.can_claim_prize(raffle, user_profile)
-
-        self.check_user_permissions(raffle, user_profile)
+        validator = ClaimPrizeValidator(
+            user_profile=user_profile,
+            raffle=raffle
+        )
+        
+        validator.is_valid(self.request.data)
 
         signature = raffle.generate_signature(
             user_profile.wallets.get(wallet_type=NetworkTypes.EVM).address)
