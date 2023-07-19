5d7c5810da0320f185073d5e2e21f92da7156b31
Shayan Shiravani <shayan5420@yahoo.com>
2023-07-19 19:51:39 +0330

    Add type to UserConstraint

--- core/models.py
+++ core/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from .constraints import *
+from django.utils.translation import gettext_lazy as _
 
 class UserConstraint(models.Model):
     class Meta:
         abstract = True
+
+    class Type(models.TextChoices):
+        VERIFICATION = "VER", _("Verification")
+        TIME = "TIME", _("Time")
 
     constraints = [
         BrightIDMeetVerification,
@@ -13,6 +18,11 @@ class UserConstraint(models.Model):
 
     name = models.CharField(max_length=255, unique=True, 
                             choices=[(c.__name__, c.__name__) for c in constraints])
+    type = models.CharField(
+        max_length=10,
+        choices=Type.choices,
+        default=Type.VERIFICATION
+    )
     response = models.TextField(null=True, blank=True)
 
     def __str__(self) -> str:
