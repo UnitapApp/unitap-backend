236cada43e7755090ae9c25fbea6772f90e957a4
Mohamad Bastin <mohamadbastin@gmail.com>
2023-10-11 01:08:14 +0200

    ad

--- prizetap/admin.py
+++ prizetap/admin.py
@@ -18,7 +18,16 @@ class RaffleٍEntryAdmin(admin.ModelAdmin):
     def get_wallet(self, obj):
         return obj.user_profile.wallets.get(wallet_type=NetworkTypes.EVM).address
 
+class LineaRaffleEntriesAdmin(admin.ModelAdmin):
+    list_display = [
+        "pk",
+        "wallet_address",
+        "is_winner"
+    ]
+
+
 
 admin.site.register(Raffle, RaffleAdmin)
 admin.site.register(RaffleEntry, RaffleٍEntryAdmin)
 admin.site.register(Constraint, UserConstraintBaseAdmin)
+admin.site.register(LineaRaffleEntries, LineaRaffleEntriesAdmin)
