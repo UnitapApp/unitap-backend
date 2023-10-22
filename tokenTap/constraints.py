94b13bfc59a475c3b51c30890205e81d3e63660f
Mohamad Bastin <mohamadbastin@gmail.com>
2023-10-22 18:33:33 +0200

    add get token constraints

--- tokenTap/constraints.py
+++ tokenTap/constraints.py
@@ -1,16 +1,17 @@
 from core.constraints import *
 from core.utils import TimeUtils
+from faucet.constraints import OptimismHasClaimedGasInThisRound
 
 
-    
 class OncePerWeekVerification(ConstraintVerification):
     def is_observed(self, *args, **kwargs):
-        token_distribution  = kwargs["token_distribution"]
+        token_distribution = kwargs["token_distribution"]
         return not token_distribution.claims.filter(
             user_profile=self.user_profile,
             created_at__gte=TimeUtils.get_last_monday(),
         ).exists()
-    
+
+
 class OncePerMonthVerification(ConstraintVerification):
     def is_observed(self, *args, **kwargs):
         token_distribution = kwargs["token_distribution"]
