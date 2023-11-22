95e15714e7286b5248e11b754aa1bd58584caa34
Shayan Shiravani <shayan5420@yahoo.com>
2023-11-22 09:30:06 +0330

    Constraints explanation

--- core/serializers.py
+++ core/serializers.py
@@ -11,13 +11,24 @@ class UserConstraintBaseSerializer(serializers.Serializer):
     title = serializers.CharField()
     type = serializers.ChoiceField(choices=UserConstraint.Type.choices)
     description = serializers.CharField()
+    explanation = serializers.CharField()
     response = serializers.CharField()
     icon_url = serializers.CharField()
     params = serializers.SerializerMethodField()
 
     class Meta:
-        fields = ["pk", "name", "title", "type", "description", "response", "icon_url", "params"]
-        read_only_fields = ["pk", "name", "title", "type", "description", "response", "icon_url", "params"]
+        fields = ["pk", "name", "title", "type", "description", "explanation", "response", "icon_url", "params"]
+        read_only_fields = [
+            "pk",
+            "name",
+            "title",
+            "type",
+            "description",
+            "explanation",
+            "response",
+            "icon_url",
+            "params",
+        ]
 
     def get_params(self, constraint: UserConstraint):
         c_class: ConstraintVerification = get_constraint(constraint.name)
