a1e1949342a210b54f846c0fef70862ec0dcbec2
Mohamad Bastin <mohamadbastin@gmail.com>
2023-12-16 16:17:31 +0100

    change single-claim cutoff time to 12 december

--- faucet/views.py
+++ faucet/views.py
@@ -104,9 +104,9 @@ class ListOneTimeClaims(ListAPIView):
                 ClaimReceipt.PENDING,
                 ClaimReceipt.REJECTED,
             ],
-            # Monday 12 December 2023
-            datetime__lt=datetime.datetime(
-                2023, 12, 12, 0, 0, 0, 0, pytz.timezone("UTC")
+            # 1 December 2023
+            datetime__gte=datetime.datetime(
+                2023, 12, 1, 0, 0, 0, 0, pytz.timezone("UTC")
             ),  # also change in credit_strategy.py
         ).order_by("-pk")
 
