34c556af4c9a208aca1fc26a9fd952c2c93d3b9a
Shayan Shiravani <shayan5420@yahoo.com>
2023-12-27 18:31:08 +0330

    Add status to raffle admin

--- prizetap/admin.py
+++ prizetap/admin.py
@@ -1,30 +1,29 @@
 from django.contrib import admin
-from prizetap.models import *
+
 from core.admin import UserConstraintBaseAdmin
+from core.models import NetworkTypes
+from prizetap.models import Constraint, LineaRaffleEntries, Raffle, RaffleEntry
 
 
 class RaffleAdmin(admin.ModelAdmin):
-    list_display = ["pk", "name", "creator_name"]
+    list_display = ["pk", "name", "creator_name", "status"]
+
 
 class RaffleٍEntryAdmin(admin.ModelAdmin):
     list_display = [
-        "pk", 
-        "raffle", 
+        "pk",
+        "raffle",
         "get_wallet",
         "age",
     ]
 
-    @admin.display(ordering='user_profile__wallets', description='Wallet')
+    @admin.display(ordering="user_profile__wallets", description="Wallet")
     def get_wallet(self, obj):
         return obj.user_profile.wallets.get(wallet_type=NetworkTypes.EVM).address
 
-class LineaRaffleEntriesAdmin(admin.ModelAdmin):
-    list_display = [
-        "pk",
-        "wallet_address",
-        "is_winner"
-    ]
 
+class LineaRaffleEntriesAdmin(admin.ModelAdmin):
+    list_display = ["pk", "wallet_address", "is_winner"]
 
 
 admin.site.register(Raffle, RaffleAdmin)
