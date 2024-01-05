0d0f9c2646b95c69252ef9bd314ce70b911f07b2
Mohamad Bastin <mohamadbastin@gmail.com>
2024-01-05 22:11:15 +0100

    verify login signature for add wallet

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,7 +1,9 @@
+import json
+
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-from authentication.helpers import verify_signature_eth_scheme
+from authentication.helpers import verify_login_signature
 from authentication.models import UserProfile, Wallet
 
 
@@ -29,16 +31,18 @@ class WalletSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Wallet
-        fields = ["pk", "wallet_type", "address", "signature", "message"]
+        fields = ["pk", "wallet_type", "wallet_address", "signature", "message"]
 
     def is_valid(self, raise_exception=False):
         super_is_validated = super().is_valid(raise_exception)
 
-        address = self.validated_data.get("address")
+        address = self.validated_data.get("wallet_address")
         message = self.validated_data.get("message")
         signature = self.validated_data.get("signature")
 
-        signature_is_valid = verify_signature_eth_scheme(address, message, signature)
+        signature_is_valid = verify_login_signature(
+            address, json.loads(message), signature
+        )
 
         if not signature_is_valid and raise_exception:
             raise serializers.ValidationError("Signature is not valid")
