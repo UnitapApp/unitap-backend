c75604296f23bc3f4d00ca0ba8bb52ee710e2b8e
reza omidian <romidian70@gmail.com>
2024-04-20 13:02:41 +0330

    change scenario from append to update

--- analytics/models.py
+++ analytics/models.py
@@ -3,8 +3,11 @@ import uuid
 from django.core.cache import cache
 from django.db import models
 
-from authentication.models import BrightIDConnection, GitcoinPassportConnection, UserProfile
-
+from authentication.models import (
+    BrightIDConnection,
+    GitcoinPassportConnection,
+    UserProfile,
+)
 
 
 class UserAnalytics(models.Model):
@@ -15,10 +18,27 @@ class UserAnalytics(models.Model):
 
     @staticmethod
     def updateData():
-        all_users_count = UserProfile.user_count()
-        brightid_users_count = BrightIDConnection.objects.all().count()
-        gitcoinpassport_users_count = GitcoinPassportConnection.objects.all().count()
-        UserAnalytics.objects.create(all_users_count=all_users_count, brightid_users_count=brightid_users_count, gitcoinpassport_users_count=gitcoinpassport_users_count)
-
-        
-
+        # Check if there's an existing record
+        if UserAnalytics.objects.exists():
+            # If record exists, update it
+            analytics = (
+                UserAnalytics.objects.first()
+            )  # Assuming there's only one record
+            analytics.all_users_count = UserProfile.user_count()
+            analytics.brightid_users_count = BrightIDConnection.objects.all().count()
+            analytics.gitcoinpassport_users_count = (
+                GitcoinPassportConnection.objects.all().count()
+            )
+            analytics.save()
+        else:
+            # If no record exists, create a new one
+            all_users_count = UserProfile.user_count()
+            brightid_users_count = BrightIDConnection.objects.all().count()
+            gitcoinpassport_users_count = (
+                GitcoinPassportConnection.objects.all().count()
+            )
+            UserAnalytics.objects.create(
+                all_users_count=all_users_count,
+                brightid_users_count=brightid_users_count,
+                gitcoinpassport_users_count=gitcoinpassport_users_count,
+            )
