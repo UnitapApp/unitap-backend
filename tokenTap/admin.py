de2b015046571056f752eb6074331de94584323b
Pooya Fekri <pooyafekri79@gmail.com>
2024-05-07 22:32:18 +0330

    Add user_wallet_address to TokenDistributionClaimAdmin

--- tokenTap/admin.py
+++ tokenTap/admin.py
@@ -32,8 +32,10 @@ class TokenDistributionClaimAdmin(admin.ModelAdmin):
         "status",
         "user_profile",
         "age",
+        "user_wallet_address",
     ]
     list_filter = ["token_distribution", "status"]
+    search_fields = ["user_wallet_address"]
 
 
 class GlobalSettingsAdmin(admin.ModelAdmin):
