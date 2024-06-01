6720e620f53d12fa6b224ab99b93f4378ab7a4f7
adamvizly <asghari.ali10@gmail.com>
2024-06-01 15:24:15 +0330

    add twitter constranits test

--- authentication/thirdpartydrivers/twitter.py
+++ authentication/thirdpartydrivers/twitter.py
@@ -1,5 +1,14 @@
 from authentication.thirdpartydrivers.abstract import BaseThirdPartyDriver
+from core.thirdpartyapp import TwitterUtils
 
 
 class TwitterDriver(BaseThirdPartyDriver):
-    pass
+    def get_tweet_count(
+        self, access_token: str, access_token_secret: str
+    ) -> None | str:
+        twitter = TwitterUtils(access_token, access_token_secret)
+        return twitter.get_tweet_count()
+
+    def get_follower_count(self, access_token: str, access_token_secret: str) -> str:
+        twitter = TwitterUtils(access_token, access_token_secret)
+        return twitter.get_follower_count()
