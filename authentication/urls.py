d8435f829fbefb3a23bd9df66fee1d09ea382e3f
Pooya Fekri <pooyafekri79@gmail.com>
2024-06-26 18:48:33 +0330

    Add farcaster connection

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,6 +7,8 @@ from authentication.views import (
     ConnectBrightIDView,
     ENSConnectionView,
     ENSDisconnectionView,
+    FarcasterConnectionView,
+    FarcasterDisconnectionView,
     GetProfileView,
     GitcoinPassportConnectionView,
     LoginRegisterView,
@@ -92,5 +94,15 @@ urlpatterns = [
         "user/disconnect/ens/<int:pk>/",
         ENSDisconnectionView.as_view(),
         name="disconnect-ens",
+    ),
+    path(
+        "user/connect/farcaster/",
+        FarcasterConnectionView.as_view(),
+        name="connect-farcaster",
+    ),
+    path(
+        "user/disconnect/farcaster/<int:pk>/",
+        FarcasterDisconnectionView.as_view(),
+        name="disconnect-farcaster",
     ),
 ]
