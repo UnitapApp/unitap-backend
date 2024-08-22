1fd2dc96b498eba0274673226b663ee58ea0f19f
Ali Maktabi <maktabi876@gmail.com>
2024-08-22 14:30:28 +0330

    removed quiztap extra codes

--- brightIDfaucet/urls.py
+++ brightIDfaucet/urls.py
@@ -29,6 +29,5 @@ urlpatterns = [
     path("api/auth/", include("authentication.urls")),
     path("api/tokentap/", include("tokenTap.urls")),
     path("api/prizetap/", include("prizetap.urls")),
-    path("api/quiztap/", include("quiztap.urls")),
     path("api/analytics/", include("analytics.urls")),
 ]
