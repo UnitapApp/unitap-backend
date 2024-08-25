2d542698a47328e4eee7de11d8f0f1ba71862cf2
Ali Maktabi <maktabi876@gmail.com>
2024-08-25 15:50:10 +0330

    added error handling on cloudflare api

--- core/thirdpartyapp/cloudflare.py
+++ core/thirdpartyapp/cloudflare.py
@@ -1,6 +1,9 @@
+import logging
 from django.conf import settings
 import requests
 
+
+logger = logging.getLogger(__name__)
 
 class CloudflareUtil:
   def __init__(self) -> None:
@@ -9,13 +12,18 @@ class CloudflareUtil:
 
 
   def is_verified(self, token: str, ip: str) -> bool:
-    res = requests.post(
-        f"{self.api_url}/siteverify",
-        data={
-            "secret": self.secret_key,
-            "response": token,
-            "remoteip": ip
-        },
-    )
+    try:
+      res = requests.post(
+          f"{self.api_url}/siteverify",
+          data={
+              "secret": self.secret_key,
+              "response": token,
+              "remoteip": ip
+          },
+      )
 
-    return res.ok and res.json()["success"]
+      return res.ok and res.json()["success"]
+    except Exception as e:
+      logger.info(f"Error occurred during cloudflare verification {str(e)}")
+
+      return False
