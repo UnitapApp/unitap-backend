1dbfb7c34e3a70c1c62879df88decdc30bbf236a
Ali Maktabi <maktabi876@gmail.com>
2024-10-13 15:53:30 +0330

    added autocomplete fields for admin panel

--- prizetap/admin.py
+++ prizetap/admin.py
@@ -7,9 +7,10 @@ from prizetap.models import Constraint, LineaRaffleEntries, Raffle, RaffleEntry
 class RaffleAdmin(admin.ModelAdmin):
     list_display = ["pk", "name", "creator_name", "status"]
     readonly_fields = ["vrf_tx_hash"]
+    autocomplete_fields = ["creator_profile"]
 
 
-class RaffleٍEntryAdmin(admin.ModelAdmin):
+class RaffleEntryAdmin(admin.ModelAdmin):
     list_display = [
         "pk",
         "raffle",
@@ -17,13 +18,15 @@ class RaffleٍEntryAdmin(admin.ModelAdmin):
         "tx_hash",
         "age",
     ]
+    autocomplete_fields = ["wallet_address"]
 
 
 class LineaRaffleEntriesAdmin(admin.ModelAdmin):
     list_display = ["pk", "wallet_address", "is_winner"]
+    autocomplete_fields = ["wallet_address"]
 
 
 admin.site.register(Raffle, RaffleAdmin)
-admin.site.register(RaffleEntry, RaffleٍEntryAdmin)
+admin.site.register(RaffleEntry, RaffleEntryAdmin)
 admin.site.register(Constraint, UserConstraintBaseAdmin)
 admin.site.register(LineaRaffleEntries, LineaRaffleEntriesAdmin)
